aaf8773d45014e458dec2f1699d880821f451f79
Muh Zaqi <zaqiikhwan22@gmail.com>
2022-06-16 22:50:31 +0700

    pertemuan4

---
+++ pertemuan4/date.php
@@ -0,0 +1,26 @@
+<?php
+    // Date
+    // Untuk menampilkan tanggal dengan format tertentu
+    // echo date("l, d-M-Y");
+
+    // Time
+    // UNIX Timestamp
+    // EPOCH time
+    // detik uamh sudah berlalu sejak 1 Januari 1970
+    // echo time();
+
+    // echo date("l", time() + 60*60*24*99);
+
+    // mktime
+    // membuat sendiri detik
+    // mktime(0, 0, 0, 0, 0, 0, 0)
+    // jam, menit, detik, bulan, tanggal, tahun
+    // echo date ("l", mktime(0,0,0,10,22,2003))
+
+    // strtotime
+    // echo date("l", strtotime("262 oct 2003"));
+
+    // String
+    // User-defined Function
+    
+?>
