2c44e41a47e6afb8d844b3da135814dc9612626e
Muh Zaqi <zaqiikhwan22@gmail.com>
2022-06-25 11:37:01 +0700

    add pertemuan11 (update)

---
+++ pertemuan11/functions.php
@@ -0,0 +1,70 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "phpdasar"); 
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data) {
+    global $conn;
+    $judul = htmlspecialchars($data["judul"]);
+    $pengarang = htmlspecialchars($data["pengarang"]);
+    $penerbit = htmlspecialchars($data["penerbit"]);
+    $tahun = htmlspecialchars($data["tahun_terbit"]);
+    $halaman = htmlspecialchars($data["halaman"]);
+    $cover = htmlspecialchars($data["cover"]);
+
+    $query = "INSERT INTO novel VALUES
+    (
+        '', 
+        '$judul', 
+        '$pengarang', 
+        '$penerbit',
+        '$tahun', 
+        '$halaman', 
+        '$cover'
+    )
+    ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM novel WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data) {
+    global $conn;
+    $id = $data["id"];
+    $judul = htmlspecialchars($data["judul"]);
+    $pengarang = htmlspecialchars($data["pengarang"]);
+    $penerbit = htmlspecialchars($data["penerbit"]);
+    $tahun = htmlspecialchars($data["tahun_terbit"]);
+    $halaman = htmlspecialchars($data["halaman"]);
+    $cover = htmlspecialchars($data["cover"]);
+
+    $query = "UPDATE novel SET 
+        judul = '$judul', 
+        pengarang = '$pengarang', 
+        penerbit = '$penerbit',
+        tahun_terbit = '$tahun', 
+        halaman = '$halaman', 
+        cover = '$cover'
+        WHERE id = $id
+    ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+?>
---
+++ pertemuan11/index.php
@@ -0,0 +1,51 @@
+<?php
+// koneksi ke database
+require 'functions.php';
+$novel = query("SELECT * FROM novel");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Novel</h1>
+
+    <a href="tambah.php">Tambah Data Novel</a>
+    <br><br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <td>No.</td>
+            <td>Aksi</td>
+            <td>Cover</td>
+            <td>Judul</td>
+            <td>Pengarang</td>
+            <td>Penerbit</td>
+            <td>Tahun Terbit</td>
+            <td>Jumlah Halaman</td>
+        </tr>
+        <?php $i = 1 ?>
+        <?php foreach ($novel as $row): ?>
+        <tr>
+            <td><?= $i;?>.</td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"];?>">ubah</a> |
+                <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('yakin?');">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["cover"];?>"></td>
+            <td><?= $row["judul"]?></td>
+            <td><?= $row["pengarang"]?></td>
+            <td><?= $row["penerbit"]?></td>
+            <td><?= $row["tahun_terbit"]?></td>
+            <td><?= $row["halaman"]?> Halaman</td>
+        </tr>
+        <?php $i++;?>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
